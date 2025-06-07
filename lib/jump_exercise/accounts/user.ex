cc175d6b3011620b32d6025f41391ee9e2b33c9b
KingdomCoder <kingdomcoding@gmail.com>
2025-06-07 18:27:49 +0100

    feat: scaffold google oauth

--- lib/jump_exercise/accounts/user.ex
+++ lib/jump_exercise/accounts/user.ex
@@ -20,6 +20,42 @@ defmodule JumpExercise.Accounts.User do
       store_all_tokens? true
       require_token_presence_for_authentication? true
     end
+
+    strategies do
+      oauth2 :google do
+        client_id System.get_env("GOOGLE_CLIENT_ID")
+        redirect_uri System.get_env("GOOGLE_REDIRECT_URI")
+        client_secret System.get_env("GOOGLE_CLIENT_SECRET")
+        user_url System.get_env("GOOGLE_USER_URL")
+        token_url System.get_env("GOOGLE_TOKEN_URL")
+        authorize_url System.get_env("GOOGLE_AUTHORIZE_URL")
+        base_url System.get_env("GOOGLE_BASE_URL")
+        authorization_params scope: "openid profile email"
+      end
+    end
+  end
+
+  actions do
+    defaults [:read]
+
+    create :register_with_google do
+      argument :user_info, :map, allow_nil?: false
+      argument :oauth_tokens, :map, allow_nil?: false
+      upsert? true
+      upsert_identity :unique_email
+
+      change AshAuthentication.GenerateTokenChange
+      change AshAuthentication.Strategy.OAuth2.IdentityChange
+
+      change fn changeset, _ ->
+        user_info = Ash.Changeset.get_argument(changeset, :user_info)
+        Ash.Changeset.change_attribute(changeset, :email, Map.get(user_info, "email"))
+      end
+
+      # change set_attribute(:confirmed_at, DateTime.utc_now())
+
+      change after_action(fn _changeset, user, _context -> {:ok, user} end)
+    end
   end
 
   policies do
