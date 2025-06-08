f81e05d79832aa3b8aba7172dbe3617d634e4448
KingdomCoder <kingdomcoding@gmail.com>
2025-06-08 20:50:06 +0100

    feat: update gmail api

--- lib/jump_exercise/gmail/gmail_api.ex
+++ lib/jump_exercise/gmail/gmail_api.ex
@@ -12,93 +12,74 @@ defmodule JumpExercise.GmailApi do
 
     messages = Jason.decode!(body)["messages"] || []
 
-    Enum.map(messages |> Enum.take(5), fn %{"id" => id} ->
-      {:ok, %HTTPoison.Response{body: msg_body, status_code: 200}} =
-        HTTPoison.get("https://gmail.googleapis.com/gmail/v1/users/me/messages/#{id}", headers)
+    emails =
+      Enum.map(messages, fn %{"id" => id} ->
+        {:ok, %HTTPoison.Response{body: msg_body, status_code: 200}} =
+          HTTPoison.get(
+            "https://gmail.googleapis.com/gmail/v1/users/me/messages/#{id}?format=full",
+            headers
+          )
 
-      Jason.decode!(msg_body)
-    end)
+        email = Jason.decode!(msg_body)
+
+        %{
+          thread_id: email["threadId"],
+          from: get_header(email, "From"),
+          to: get_header(email, "To"),
+          subject: get_header(email, "Subject"),
+          body: extract_body(email),
+          labels: email["labelIds"] || [],
+          snippet: email["snippet"] || "",
+          raw: Jason.encode!(email)
+        }
+      end)
+
+    emails
   end
 
-  def get_email(user, id) do
-    access_token = get_access_token_for_user(user)
-
-    headers = [
-      {"Authorization", "Bearer #{access_token}"},
-      {"Accept", "application/json"}
-    ]
-
-    url = "https://gmail.googleapis.com/gmail/v1/users/me/messages/#{id}"
-
-    case HTTPoison.get(url, headers) do
-      {:ok, %HTTPoison.Response{body: body, status_code: 200}} ->
-        {:ok, Jason.decode!(body)}
-
-      error ->
-        error
+  defp get_header(%{"payload" => %{"headers" => headers}}, name) do
+    headers
+    |> Enum.find(fn %{"name" => n} -> n == name end)
+    |> case do
+      %{"value" => value} -> value
+      _ -> nil
     end
   end
 
-  def create_email(user, email_params) do
-    access_token = get_access_token_for_user(user)
+  defp extract_body(%{"payload" => payload}), do: extract_body_from_payload(payload)
+  defp extract_body(_), do: ""
 
-    headers = [
-      {"Authorization", "Bearer #{access_token}"},
-      {"Content-Type", "application/json"}
-    ]
+  defp extract_body_from_payload(%{"mimeType" => "text/plain", "body" => %{"data" => data}})
+       when is_binary(data) do
+    data
+    |> String.replace("-", "+")
+    |> String.replace("_", "/")
+    |> Base.decode64!(ignore: :whitespace)
+  end
 
-    url = "https://gmail.googleapis.com/gmail/v1/users/me/messages/send"
-    body = Jason.encode!(email_params)
+  defp extract_body_from_payload(%{"parts" => parts}) when is_list(parts) do
+    plain =
+      Enum.find(parts, fn part -> part["mimeType"] == "text/plain" end)
+      |> case do
+        nil -> nil
+        part -> extract_body_from_payload(part)
+      end
 
-    case HTTPoison.post(url, body, headers) do
-      {:ok, %HTTPoison.Response{body: body, status_code: 200}} ->
-        {:ok, Jason.decode!(body)}
+    if plain do
+      plain
+    else
+      html =
+        Enum.find(parts, fn part -> part["mimeType"] == "text/html" end)
+        |> case do
+          nil -> nil
+          part -> extract_body_from_payload(part)
+        end
 
-      error ->
-        error
+      html || ""
     end
   end
 
-  def update_email_labels(user, id, label_ids) do
-    access_token = get_access_token_for_user(user)
-
-    headers = [
-      {"Authorization", "Bearer #{access_token}"},
-      {"Content-Type", "application/json"}
-    ]
-
-    url = "https://gmail.googleapis.com/gmail/v1/users/me/messages/#{id}/modify"
-    body = Jason.encode!(%{addLabelIds: label_ids})
-
-    case HTTPoison.post(url, body, headers) do
-      {:ok, %HTTPoison.Response{body: body, status_code: 200}} ->
-        {:ok, Jason.decode!(body)}
-
-      error ->
-        error
-    end
-  end
-
-  def delete_email(user, id) do
-    access_token = get_access_token_for_user(user)
-
-    headers = [
-      {"Authorization", "Bearer #{access_token}"},
-      {"Accept", "application/json"}
-    ]
-
-    url = "https://gmail.googleapis.com/gmail/v1/users/me/messages/#{id}"
-
-    case HTTPoison.delete(url, headers) do
-      {:ok, %HTTPoison.Response{status_code: 204}} ->
-        :ok
-
-      error ->
-        error
-    end
-  end
-
-  defp get_access_token_for_user(nil), do: nil
+  defp extract_body_from_payload(_), do: ""
 
   defp get_access_token_for_user(user) do
     {:ok, user} = Ash.load(user, :identities)
